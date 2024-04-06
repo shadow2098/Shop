8b0bb51573eab1ce6d6ae9ad2d4c25a01ae0d9c7
shadow2098 <sulejmangasymov880@gmail.com>
2024-04-06 16:18:10 +0300

    views, urls, and templates are functional

--- store_app/urls.py
+++ store_app/urls.py
@@ -1,1 +1,9 @@
-urls.py
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.return_store, name='store'),
+    path('shopping_cart/', views.return_shopping_cart, name='shopping_cart'),
+    path('checkout/', views.return_checkout, name='checkout'),
+]
--- store_app/views.py
+++ store_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def return_store(request):
+    context = {}
+    return render(request, 'store_app/store.html', context)
+
+
+def return_shopping_cart(request):
+    return render(request, 'store_app/shopping_cart.html', {})
+
+
+def return_checkout(request):
+    return render(request, 'store_app/checkout.html', {})
