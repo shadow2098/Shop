f264293fbde0b7ad369b9e73f433bada53545619
shadow2098 <sulejmangasymov880@gmail.com>
2024-04-20 20:10:27 +0300

    Now both anonymous and logged in users can create and finish the order process

--- store_app/views.py
+++ store_app/views.py
@@ -77,24 +77,51 @@ def process_order(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        total = float(data['form']['total'])
-        order.transaction_id = transaction_id
 
-        if total == float(order.get_cart_price):
-            order.complete = True
-        order.save()
-
-        if order.shipping == True:
-            ShippingAddress.objects.create(
-                customer = customer,
-                order = order,
-                address = data['shipping']['address'],
-                city = data['shipping']['city'],
-                state = data['shipping']['state'],
-                zipcode = data['shipping']['zipcode']
-            )
     else:
         print("User is not logged in")
+        print("Cookies:", request.COOKIES)
+
+        name = data['form']['name']
+        email = data['form']['email']
+
+        cookie_data = cookie_cart(request)
+        items = cookie_data['items']
+
+        customer, created = Customer.objects.get_or_create(email=email)
+        customer.name = name
+        customer.save()
+
+        order = Order.objects.create(
+            customer=customer,
+            complete=False,
+            )
+
+        for item in items:
+            product = Product.objects.get(id=item['product']['id'])
+
+            order_item = OrderItem.objects.create(
+                product=product,
+                order=order,
+                quantity=item['quantity']
+                )
+
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
+
+    if total == float(order.get_cart_price):
+        order.complete = True
+        order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer = customer,
+            order = order,
+            address = data['shipping']['address'],
+            city = data['shipping']['city'],
+            state = data['shipping']['state'],
+            zipcode = data['shipping']['zipcode']
+        )
     return JsonResponse('Payment was done', safe=False)
 '''
 @csrf_protect
